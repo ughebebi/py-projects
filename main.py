92c8279412b2ed74d0735a57a8963023c4516882
ughebebi <328412341+ughebebi@users.noreply.github.com>
2026-09-24 22:46:34 +0300

    начал писать лаунчер.

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-# from cash_register import *
+from cash_register import cash_register
+from gan import *
 
+print("  добро пожаловать в игру  ")
+print(" 1 - игра \"угадай число\"")
+print(" 2 - \"Робот кассир\"")
+print(" 0 - выход")
+print("  Введите число")
+# todo сделать защиту
+i = int(input())
+
+if i == 0:
+    exit()
+elif i == 1:
+    gan()
+elif i == 2:
+    cash_register()
